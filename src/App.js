865936a7efab7a4f3e271c7509ea993663da43ff
Sarthakw001 <sarthakw01@gmail.com>
2023-01-12 19:13:13 +0530

    tailwindcss+postcss+daisyui+react-router-dom added and configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Homepage from './Components/Homepage';
+import Primary from './Components/Primary';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/primary' element={<Primary/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Primary.js
@@ -0,0 +1,9 @@
+const Primary = () => {
+  return (
+    <div>
+      <h1>Hello from primary</h1>
+    </div>
+  );
+};
+
+export default Primary;
